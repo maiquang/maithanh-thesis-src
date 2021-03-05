399e8fe13e1f3014d9dfc3a27638c92dc70bc140
Thanh Quang Mai <quangies7@gmail.com>
2021-03-05 23:49:41 +0100

    Trajectory - distinct observation for each observer

--- src/trajectory.py
+++ src/trajectory.py
@@ -35,7 +35,9 @@ class Trajectory:
         Simulate trajectory.
     """
 
-    def __init__(self, model, n_steps, init_state=None, random_seed=None, u=None):
+    def __init__(
+        self, model, n_steps, n_obs=1, init_state=None, random_seed=None, u=None
+    ):
         """Initialize trajectory simulator and simulate n_steps.
 
         Parameters
@@ -44,6 +46,8 @@ class Trajectory:
             State-space representation model
         n_steps : int
             Number of simulation steps
+        n_obs : int
+            Number of distinct observations in each step
         init_state : np.array, optional
             Initial state vector at t=0, default is a zero vector
         random_seed : int, optional
@@ -59,9 +63,9 @@ class Trajectory:
         if init_state is None:
             init_state = np.zeros(self.model.A.shape[0], dtype=np.float64)
 
-        self.X, self.Y = self.simulate(n_steps, init_state, random_seed, u)
+        self.X, self.Y = self.simulate(n_steps, n_obs, init_state, random_seed, u)
 
-    def simulate(self, n_steps, init_state=None, random_seed=None, u=None):
+    def simulate(self, n_steps, n_obs=1, init_state=None, random_seed=None, u=None):
         """ Simulate trajectory.
 
         Parameters
@@ -89,7 +93,7 @@ class Trajectory:
             np.random.seed(random_seed)
 
         X = np.zeros(shape=(n_steps, self.model.A.shape[0]))
-        Y = np.zeros(shape=(n_steps, self.model.H.shape[0]))
+        Y = np.zeros(shape=(n_steps, n_obs, self.model.H.shape[0]))
 
         x = init_state
         for t in range(n_steps):
@@ -97,12 +101,12 @@ class Trajectory:
             if self.model.B is not None and u is not None:
                 x += self.model.B.dot(u)
 
-            y = self.model.H.dot(x) + mvn.rvs(cov=self.model.R)
+            y = self.model.H.dot(x) + mvn.rvs(cov=self.model.R, size=n_obs)
 
             X[t, :] = x
             Y[t, :] = y
 
-        return X, Y
+        return X, np.squeeze(Y)
 
     @property
     def states(self):
