e93b5879f303139bc8b3d6d48fa1b47731c23117
Thanh Quang Mai <quangies7@gmail.com>
2021-03-07 00:40:34 +0100

    Basic plotting function

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+from matplotlib import pyplot as plt
 
 
 def rmse(y, x, n):
@@ -19,3 +20,31 @@ def rmse(y, x, n):
     """
     t = np.arange(1, n + 1)
     return np.sqrt(((y[:n, 0:2] - x[:n, 0:2]) ** 2).cumsum(axis=1) / t.reshape(n, 1))
+
+
+def plot_estimates(est, traj, n=None, labels=None):
+    hist = est.history
+    n = n if n is not None else hist.shape[1]
+
+    plt.figure(figsize=(12, n * 4))
+    for i in range(n):
+        plt.subplot(n, 1, i + 1)
+        plt.plot(hist[:, i])
+        plt.plot(traj.X[:, i])
+        plt.xlabel("$t$", fontsize="large")
+        plt.vlines(
+            est._reset_log,
+            np.min([hist[:, i], traj.X[:, i]]),
+            np.max([hist[:, i], traj.X[:, i]]),
+            colors="g",
+            linestyles="dashed",
+            alpha=0.7,
+        )
+
+        try:
+            plt.ylabel(labels[i])
+        except:
+            pass
+
+    plt.figlegend(["Estimate", "Real value", "Filter reset"])
+    plt.show()
