239e5a3410fc072946cab0c74fda380717b9a967
Thanh Quang Mai <quang@pop-os.localdomain>
2021-06-09 01:33:16 +0200

    Trajectory - add function to generate observation noise

--- src/trajectory.py
+++ src/trajectory.py
@@ -5,7 +5,7 @@ from .statespace import StateSpace
 
 
 class Trajectory:
-    """ Trajectory simulator class.
+    """Trajectory simulator class.
 
     Parameters
     ----------
@@ -13,10 +13,10 @@ class Trajectory:
         State-space representation model
     n_steps : int
         Number of simulation steps
-    n_obs : int
-        Number of distinct observations in each step
     init_state : np.array, optional
         Initial state vector at t=0, default is a zero vector
+    R : list of matrices/scalars, optional
+        List of observation noise covariance matrices, by default "model"
     random_seed : int, optional
         Random seed for PRNG initialization
     u : np.array, optional
@@ -26,19 +26,21 @@ class Trajectory:
     ----------
     model : StateSpace object
         State-space representation model
-    X : np.array
+    states : np.array
         An array of simulated states
-    Y : np.array
+    observations : np.array
         An array of simulated observations
 
     Methods
     -------
     simulate(n_steps, init_state=None, random_seed=None, u=None)
         Simulate trajectory.
+    add_obs_noise(R="model", random_seed=None)
+        Add observation noise.
     """
 
     def __init__(
-        self, model, n_steps, init_state=None, R=None, random_seed=None, u=None
+        self, model, n_steps, init_state=None, R="model", random_seed=None, u=None
     ):
         """Initialize trajectory simulator and simulate n_steps.
 
@@ -50,8 +52,8 @@ class Trajectory:
             Number of simulation steps
         init_state : np.array, optional
             Initial state vector at t=0, default is a zero vector
-        R :
-
+        R : list of matrices/scalars, optional
+            List of observation noise covariance matrices, by default "model"
         random_seed : int, optional
             Random seed for PRNG initialization
         u : np.array, optional
@@ -65,49 +67,33 @@ class Trajectory:
         if init_state is None:
             init_state = np.zeros(self.model.A.shape[0], dtype=np.float64)
 
-        self.X, self.Y = self.simulate(n_steps, init_state, R, u, random_seed)
+        if random_seed is not None:
+            np.random.seed(random_seed)
 
-    def simulate(self, n_steps, init_state=None, R=None, u=None, random_seed=None):
-        """ Simulate trajectory.
+        self.simulate(n_steps, init_state, u)
+        if R is not None:
+            self.add_obs_noise(R)
+
+    def simulate(self, n_steps, init_state=None, u=None, random_seed=None):
+        """Simulate a new trajectory, can be retrieves from the attribute "X"
+        or "states".
 
         Parameters
         ----------
-        model : StateSpace object
-            State-space representation model
         n_steps : int
             Number of simulation steps
-        n_obs : int
-            Number of distinct observations in each step
         init_state : np.array, optional
             Initial state vector at t=0, default is a zero vector
-        random_seed : int, optional
-            Random seed for PRNG initialization
         u : np.array, optional
             Control vector
-
-        Returns
-        -------
-        X : np.array
-            An array of simulated states
-        Y : np.array
-            An array of simulated observations
+        random_seed : int, optional
+            Random seed for PRNG initialization, by default None
         """
 
         if random_seed is not None:
             np.random.seed(random_seed)
 
-        if R is None:
-            R = self.model.R
-
-        if R.ndim == 1:
-            # List of scalars
-            R = np.atleast_3d(R)
-        elif R.ndim == 2:
-            # One matrix
-            R = R[np.newaxis, :]
-
         X = np.zeros(shape=(n_steps, self.model.A.shape[0]))
-        Y = np.zeros(shape=(n_steps, R.shape[0], self.model.H.shape[0]))
 
         # Generate true states
         x = init_state
@@ -117,14 +103,44 @@ class Trajectory:
                 x += self.model.B.dot(u)
             X[t, :] = x
 
+        self.X = X
+
+    def add_obs_noise(self, R="model", random_seed=None):
+        """Add observation noise to the generated trajectory. Noise is
+        generated based on the function parameter R. Observations can be
+        accessed from the attribute "observations" or "Y".
+
+        Parameters
+        ----------
+        R : list of matrices/scalars, optional
+            List of observation noise covariance matrices, by default "model"
+        random_seed : int, optional
+            Random seed for PRNG initialization, by default None
+        """
+        if random_seed is not None:
+            np.random.seed(random_seed)
+
+        if R == "model":
+            # Use observation matrix from the initialized state-space model
+            R = self.model.R
+
+        if R.ndim == 1:
+            # List of scalars
+            R = np.atleast_3d(R)
+        elif R.ndim == 2:
+            # One matrix
+            R = R[np.newaxis, :]
+
+        Y = np.zeros(shape=(self.X.shape[0], R.shape[0], self.model.H.shape[0]))
+
         # Add observation noise
         for i, r in enumerate(R):
             # y = Hx + obs.noise
-            Y[:, i] = np.einsum("ij, kj -> ki", self.model.H, X) + mvn.rvs(
-                cov=r, size=n_steps
+            Y[:, i] = np.einsum("ij, kj -> ki", self.model.H, self.X) + mvn.rvs(
+                cov=r, size=self.X.shape[0]
             )
 
-        return X, np.squeeze(Y)
+        self.Y = np.squeeze(Y)
 
     @property
     def states(self):
