29c74e97bb7b19d31f93dd866c6a61dab0dc4356
Thanh Quang Mai <quang@pop-os.localdomain>
2021-06-09 20:26:41 +0200

    Trajectory - fix bug for a list of scalars in obs noise

--- src/trajectory.py
+++ src/trajectory.py
@@ -15,8 +15,9 @@ class Trajectory:
         Number of simulation steps
     init_state : np.array, optional
         Initial state vector at t=0, default is a zero vector
-    R : list of matrices/scalars, optional
-        List of observation noise covariance matrices, by default "model"
+    R : list of matrices/scalars, None, False, optional
+        List of observation noise covariance matrices,
+        by default False (use observation matrix from the initialized state-space model)
     random_seed : int, optional
         Random seed for PRNG initialization
     u : np.array, optional
@@ -35,12 +36,12 @@ class Trajectory:
     -------
     simulate(n_steps, init_state=None, random_seed=None, u=None)
         Simulate trajectory.
-    add_obs_noise(R="model", random_seed=None)
+    add_obs_noise(R=False, random_seed=None)
         Add observation noise.
     """
 
     def __init__(
-        self, model, n_steps, init_state=None, R="model", random_seed=None, u=None
+        self, model, n_steps, init_state=None, R=False, random_seed=None, u=None
     ):
         """Initialize trajectory simulator and simulate n_steps.
 
@@ -52,8 +53,9 @@ class Trajectory:
             Number of simulation steps
         init_state : np.array, optional
             Initial state vector at t=0, default is a zero vector
-        R : list of matrices/scalars, optional
-            List of observation noise covariance matrices, by default "model"
+        R : list of matrices/scalars, None, False, optional
+            List of observation noise covariance matrices,
+            by default False (use observation matrix from the initialized state-space model)
         random_seed : int, optional
             Random seed for PRNG initialization
         u : np.array, optional
@@ -105,40 +107,43 @@ class Trajectory:
 
         self.X = X
 
-    def add_obs_noise(self, R="model", random_seed=None):
+    def add_obs_noise(self, R=False, random_seed=None):
         """Add observation noise to the generated trajectory. Noise is
         generated based on the function parameter R. Observations can be
         accessed from the attribute "observations" or "Y".
 
         Parameters
         ----------
-        R : list of matrices/scalars, optional
-            List of observation noise covariance matrices, by default "model"
+        R : list of matrices/scalars, None, False, optional
+            List of observation noise covariance matrices,
+            by default False (use observation matrix from the initialized state-space model)
         random_seed : int, optional
             Random seed for PRNG initialization, by default None
         """
         if random_seed is not None:
             np.random.seed(random_seed)
 
-        if R == "model":
+        if R is False:
             # Use observation matrix from the initialized state-space model
             R = self.model.R
 
-        if R.ndim == 1:
-            # List of scalars
-            R = np.atleast_3d(R)
-        elif R.ndim == 2:
+        if np.asarray(R).ndim == 1:
+            # List of scalars (for 1D trajectory)
+            R = np.asarray(R)[:, np.newaxis, np.newaxis]
+        elif np.asarray(R).ndim == 2:
             # One matrix
-            R = R[np.newaxis, :]
+            R = np.asarray(R)[np.newaxis, :]
 
-        Y = np.zeros(shape=(self.X.shape[0], R.shape[0], self.model.H.shape[0]))
+        Y = np.zeros(
+            shape=(self.X.shape[0], np.asarray(R).shape[0], self.model.H.shape[0])
+        )
 
         # Add observation noise
         for i, r in enumerate(R):
             # y = Hx + obs.noise
             Y[:, i] = np.einsum("ij, kj -> ki", self.model.H, self.X) + mvn.rvs(
                 cov=r, size=self.X.shape[0]
-            )
+            ).reshape((self.X.shape[0], -1))
 
         self.Y = np.squeeze(Y)
 
